2ae146546c7fd3b67d7fa72b5fb10c39fe8aae01
kplbb <katpark16@gmail.com>
2023-07-16 23:25:51 +0900

    Add: result word cloud component & css, result page

---
+++ src/components/ResultWordCloud.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import './ResultWordCloud.css'
+
+export default function ResultWordCloud() {
+  return (
+    <div>
+      <ul class="cloud" role="navigation" aria-label="Webdev word cloud">
+        <li>
+          <a href="#" data-weight="4">
+            HTTP
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="1">
+            Ember
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="5">
+            Sass
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="8">
+            HTML
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="6">
+            FlexBox
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="4">
+            API
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="5">
+            VueJS
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="6">
+            Grid
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="2">
+            Rest
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="9">
+            JavaScript
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="3">
+            Animation
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="7">
+            React
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="8">
+            CSS
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="1">
+            Cache
+          </a>
+        </li>
+        <li>
+          <a href="#" data-weight="3">
+            Less
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,12 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ResultWordCloud from '../components/ResultWordCloud'
+import loadingBot from '../assets/gpt_02.png'
+const style = {
+  display: 'block',
+}
 
+const sentenceStyle = {
+  padding: '10%',
+  backgroundColor: '#f2f7ff',
+  borderRadius: '10px',
+  marginBottom: '10px',
+}
 const Result = () => {
   return (
     <div className="container">
-      <div className="banner">result</div>
+      <div className="banner" style={{ display: 'block' }}>
+        당신의 첫인상은?
+        {/* <div style={style}> */}
+        <ResultWordCloud />
+        {/* </div> */}
+        {/* style={{ height: '200px', objectFit: 'auto' }} /> */}
+        <img
+          src="https://howtodrawforkids.com/wp-content/uploads/2017/03/how-to-draw-a-face-step-by-step.jpg"
+          style={{ width: '50%', borderRadius: '50%' }}
+        />
+        <div>
+          <img src={loadingBot} style={{ width: '30%' }} />
+          애교넘치는 호감형!
+        </div>
+        <div style={sentenceStyle}>
+          인공지능이 판단한 당신의 첫인상은 '애교넘치는 호감형' 입니다
+        </div>
+        <div style={sentenceStyle}>
+          본 테스트 결과는 전체의 0.33%에 속하며 가족, 친구 등 좋아하는 내
+          사람들과 있을 때는 '술무새 역할’을 맡고 있습니다.
+        </div>
+        <div style={sentenceStyle}>
+          또한 애기 같은 외모를 가진 당신은 천사 같은 모습으로 주위 사람들을
+          포용하는 능력을 지니고 있는 것으로 보입니다.
+        </div>
+        <div style={sentenceStyle}>
+          마지막으로 비슷한 분위기가 느껴지는 연예인은 김세정입니다.
+        </div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Result;
+export default Result
