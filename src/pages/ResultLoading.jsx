4c41ca6568da97b5c5046a884858486c45aeff9b
kplbb <katpark16@gmail.com>
2023-07-23 17:07:08 +0900

    Modify: move styles to css file and import

--- src/pages/ResultLoading.jsx
+++ src/pages/ResultLoading.jsx
@@ -1,24 +1,18 @@
 import { useNavigate } from 'react-router-dom'
 import loadingAnimation from '../assets/loading_bg.png'
 import loadingBot from '../assets/gpt_02.png'
+import './ResultLoading.css'
 const ResultLoading = () => {
   const navigate = useNavigate()
   return (
     <>
-      <div
-        className="ad"
-        style={{
-          height: '80px',
-          backgroundColor: 'gray',
-          marginBottom: '30px',
-        }}
-      ></div>
+      <div className="ad"></div>
       <div className="container">
         <div className="banner">
           ai 모델링을 통한 첫인상을 분석중입니다....
-          <img src={loadingAnimation} style={{ width: '90%' }} />
-          <img src={loadingBot} style={{ width: '70%' }} />
-          <div className="btn btn-1">Hover me</div>
+          <img src={loadingAnimation} className="loading-animation" />
+          <img src={loadingBot} className="loading-bot box bounce-1" />
+          <div className="btn btn-1">Loading...</div>
         </div>
       </div>
     </>
