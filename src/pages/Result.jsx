96a04eaf20e3a21ea585d0b1274b827852527fe1
kplbb <katpark16@gmail.com>
2023-07-24 00:09:36 +0900

    Add: copy link function, receive id as state prop

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import ResultWordCloud from '../components/ResultWordCloud'
 import loadingBot from '../assets/gpt_02.png'
@@ -6,81 +6,117 @@ import './Result.css'
 import textBubble from '../assets/text_bg.png'
 import { useLocation } from 'react-router'
 import { BsLink45Deg } from 'react-icons/bs'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 import { BsFillShareFill } from 'react-icons/bs'
 
 const Result = () => {
+  const [data, setData] = useState()
+  const [resultId, setResultId] = useState()
+
   const { state } = useLocation()
-  console.log(state)
+
+  useEffect(() => {
+    if (state != undefined || null) {
+      console.log(state)
+      setData(state)
+    }
+  }, [state])
+
+  const copyLink = (e) => {
+    e.preventDefault()
+    const currentURL = window.location.href
+    const input = document.createElement('input')
+    input.value = currentURL
+    input.style.position = 'absolute'
+    input.style.left = '-9999px'
+    document.body.appendChild(input)
+    input.select()
+    try {
+      document.execCommand('copy')
+      alert('URL copied to clipboard!')
+    } catch (error) {
+      console.error('Failed to copy URL to clipboard: ', error)
+    }
+    document.body.removeChild(input)
+  }
   return (
     <div className="container">
       <div className="banner">
-        <div className="HeadText">당신의 첫인상은?</div>
-        <ResultWordCloud />
-
-        <div className="user-img">
-          <img src={loadingBot} />
-        </div>
-
-        <div className="res-container">
-          <div className="res-img-container">
-            <img src={loadingBot} alt="Image" />
+        {/* {data !== null && data !== undefined ? ( */}
+        <>
+          <div className="HeadText">당신의 첫인상은?</div>
+          <ResultWordCloud />
+          <div className="user-img">
+            <img src={loadingBot} />
           </div>
-          <div className="res-text-container">
-            <p>애교넘치는 호감형!</p>
-          </div>
-        </div>
 
-        <div className="result-blocks">
-          인공지능이 판단한 당신의 첫인상은 '애교넘치는 호감형' 입니다
-        </div>
-        <div className="result-blocks">
-          본 테스트 결과는 전체의 0.33%에 속하며 가족, 친구 등 좋아하는 내
-          사람들과 있을 때는 '술무새 역할’을 맡고 있습니다.
-        </div>
-        <div className="result-blocks">
-          또한 애기 같은 외모를 가진 당신은 천사 같은 모습으로 주위 사람들을
-          포용하는 능력을 지니고 있는 것으로 보입니다.
-        </div>
-        <div className="result-blocks">
-          마지막으로 비슷한 분위기가 느껴지는 연예인은 김세정입니다.
-        </div>
+          <div className="res-container">
+            <div className="res-img-container">
+              <img src={loadingBot} alt="Image" />
+            </div>
+            <div className="res-text-container">
+              <p>애교넘치는 호감형!</p>
+            </div>
+          </div>
 
-        <div className="summary-container">
-          <div className="summary-left">
-            잘 맞는 유형 <br />
-            어리바리 담당
+          <div className="result-blocks">
+            인공지능이 판단한 당신의 첫인상은 '애교넘치는 호감형' 입니다
           </div>
-          <div className="summary-right">
-            안 맞는 유형 <br />
-            TMT 담당
+          <div className="result-blocks">
+            본 테스트 결과는 전체의 0.33%에 속하며 가족, 친구 등 좋아하는 내
+            사람들과 있을 때는 '술무새 역할’을 맡고 있습니다.
           </div>
-        </div>
+          <div className="result-blocks">
+            또한 애기 같은 외모를 가진 당신은 천사 같은 모습으로 주위 사람들을
+            포용하는 능력을 지니고 있는 것으로 보입니다.
+          </div>
+          <div className="result-blocks">
+            마지막으로 비슷한 분위기가 느껴지는 연예인은 김세정입니다.
+          </div>
 
-        <div className="share-container">
-          <div className="share-img-container">
-            <img src={loadingBot} alt="Image" />
+          <div className="summary-container">
+            <div className="summary-left">
+              잘 맞는 유형 <br />
+              어리바리 담당
+            </div>
+            <div className="summary-right">
+              안 맞는 유형 <br />
+              TMT 담당
+            </div>
           </div>
-          <div className="share-text-container">
-            <img src={textBubble} alt="Image" />
 
-            <p>친구들이 생각하는 내 첫인상은?</p>
+          <div className="share-container">
+            <div className="share-img-container">
+              <img src={loadingBot} alt="Image" />
+            </div>
+            <div className="share-text-container">
+              <img src={textBubble} alt="Image" />
+              <p>친구들이 생각하는 내 첫인상은?</p>
+            </div>
           </div>
-        </div>
 
-        <div className="flex-container">
-          <div className="flex-item-left">
-            내 결과 공유하기 <BsFillShareFill />
+          <div className="flex-container">
+            <div className="flex-item-left">
+              내 결과 공유하기 <BsFillShareFill />
+            </div>
           </div>
-        </div>
 
-        <section className="share-icon-container">
-          <div className="icon">
-            <BsLink45Deg style={{ height: '40px', width: '40px' }} />
-          </div>
-          <div className="icon"></div>
-          <div className="icon"></div>
-        </section>
+          <section className="share-icon-container">
+            <div className="icon">
+              <BsLink45Deg
+                style={{ height: '40px', width: '40px' }}
+                onClick={(e) => copyLink(e)}
+              />
+            </div>
+            <div className="icon"></div>
+            <div className="icon"></div>
+          </section>
+        </>
+        {/* ) : ( */}
+        {/* <div c  lassName="">no data yet</div> */}
+        {/* )} */}
       </div>
     </div>
   )
--- src/pages/ResultLoading.jsx
+++ src/pages/ResultLoading.jsx
@@ -5,51 +5,50 @@ import './ResultLoading.css'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import ErrorPage from '../components/ErrorPage'
+import { useLocation } from 'react-router'
 
-const ResultLoading = ({ id }) => {
+const ResultLoading = ({}) => {
   const [error, setError] = useState(false)
   const navigate = useNavigate()
+  const { state } = useLocation()
 
   useEffect(() => {
-    if (id != null || undefined) {
-      const fetchData = async () => {
-        try {
-          const reqUrl = ''
-          // process.env[`REACT_APP_` + process.env.REACT_APP_MODE + `_API_URL`] +
-          // url
+    const fetchData = async () => {
+      try {
+        const reqUrl = ''
+        // process.env[`REACT_APP_` + process.env.REACT_APP_MODE + `_API_URL`] +
+        // url
 
-          const requestHeaders = {
-            Authorization: 'token',
-            'Content-Type': 'application/json',
-          }
-
-          const response = await axios({
-            method: 'POST',
-            url: reqUrl,
-            headers: requestHeaders,
-            data: 'id',
-          })
-          // if response.data
-          // redirect to result page
-          // navigate('/result', { state: 'data' })
-        } catch (error) {
-          setError(true)
-          console.log(error)
+        const requestHeaders = {
+          Authorization: 'token',
+          'Content-Type': 'application/json',
         }
+
+        const response = await axios({
+          method: 'POST',
+          url: reqUrl,
+          headers: requestHeaders,
+          data: state,
+        })
+        if (response.response == 200)
+          navigate('/result', { state: 'response.data' })
+      } catch (error) {
+        setError(true)
+        console.log(error)
       }
     }
-  }, [])
+    if (state != null || undefined) {
+      fetchData()
+    }
+  })
 
-  const testFunction = () => {
-    navigate('/result', { state: 'data' })
-  }
   return (
     <>
       {error ? (
         <ErrorPage />
       ) : (
         <>
-          <div className="ad" onClick={() => testFunction()}></div>
+          <div className="ad"></div>
           <div className="container">
             <div className="banner">
               ai 모델링을 통한 첫인상을 분석중입니다....
